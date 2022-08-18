d956e0789ecfa7e74a0a18935f50d840b790286a
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-08-18 19:40:13 +0200

    schema fix

--- dashboard/models.py
+++ dashboard/models.py
@@ -25,3 +25,26 @@ class Branches(models.Model):
             "address": self.address,
             "date": self.date,
         }
+
+
+class Feedbacks(models.Model):
+    id = models.AutoField(auto_created=True, primary_key=TRUE)
+    customer_full_name = models.CharField(
+        max_length=500, blank=False, null=False)
+    email_address = models.CharField(max_length=100, blank=False, null=False)
+    phone_number = models.CharField(max_length=50, blank=False, null=False)
+    message = models.TextField()
+    date = models.DateTimeField(default=datetime.datetime.now())
+
+    # def __str__(self):
+    #     return self.name + ' - ' + self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "names": self.customer_full_name,
+            "email": self.email_address,
+            "phone_number": self.phone_number,
+            "message": self.message,
+            "date": self.date,
+        }
--- queue_management/settings.py
+++ queue_management/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pages',
     'dashboard',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
