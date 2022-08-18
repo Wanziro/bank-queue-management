baf5c5d34aebd86ac3b2e8934da307b920816ef9
Fabrice Niyongabo <ninga.fab@gmail.com>
2022-08-18 22:45:11 +0200

    socket url update

--- static/js/queueController.js
+++ static/js/queueController.js
@@ -1,5 +1,5 @@
-const URL = "http://localhost:5000";
-// const URL = "https://bank-queue-socket-server.herokuapp.com";
+// const URL = "http://localhost:5000";
+const URL = "https://bank-queue-socket-server.herokuapp.com";
 const currentTimeComponent = $("#currentTimeComponent");
 const queueContainer = $("#queueContainer");
 const queueLengthContainer = $("#queueLengthContainer");
